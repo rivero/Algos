09b953446fbf9e59bf07e044d970cf7299f14612
Jaguar Rivero <jrivero@rocketmail.com>
2024-05-13 09:10:28 -0700

    Added move_zeroes versio 2

--- LargetrParenthesis/LargetrParenthesis.cpp
+++ LargetrParenthesis/LargetrParenthesis.cpp
@@ -15,6 +15,7 @@ using namespace std;
 
 struct Solution
 {
+	// find the longest valid parenthesis
 	void FindParenthesis(const string& s)
 	{
 		int initialSquare{ INT_MAX }
@@ -22,7 +23,7 @@ struct Solution
 			, initialRound{ INT_MAX }
 		, lastSquare{ -1 }, lastCurly{ -1 }, lastround{ -1 };
 
-		for (int i = 0; i < s.size(); i++)
+		for (size_t i = 0; i < s.size(); i++)
 		{
 			if (initialSquare == INT_MAX && s[i] == '[')
 			{
@@ -64,7 +65,68 @@ struct Solution
 
 		cout << "Max length: " << masimum << endl;
 	}
+	// move all zeroes to the right side
 
+	// Soluition 1
+	// using two pointers. move left pointer forward and right pointer backwards. 
+	// if a zero is detected at the left swap it with the right element.
+	// the number of zeroes whould be returned
+	template <typename T>
+	size_t move_zeroesV1(T& c) // Collection
+	{
+		size_t l{}, r{c.size() - 1};
+		while (l < r)
+		{
+			if (c[l] == 0)
+			{
+				swap(c[l++], c[r]);
+			}
+			else
+				l++;
+			if (c[r] != 0)
+			{
+				swap(c[r--], c[l]);
+			}
+			else
+				r--;
+		}
+		return l;
+	}
+
+	// Solution 2
+	// start from the beginning, check if we have a zero.
+	// find the first non zero to its right
+	//if found swap it.
+	template <typename T>
+	size_t move_zeroesV2(T& c) // Collection
+	{
+		size_t counter{};
+		for (size_t i = 0; i < c.size(); i++)
+		{
+			if (c[i] == 0)
+			{
+				// find the first non-zero and swap it
+				for (size_t j = i + 1; j < c.size(); j++)
+				{
+					if (c[j] != 0)
+					{
+						swap(c[i], c[j]);
+						counter++;
+					}
+				}
+			}
+		}
+		return counter;
+	}
+	template <typename T>
+	void printv(const T& v) const
+	{
+		for (const auto& elem: v)
+		{
+			cout << elem << " ";
+		}
+		cout << endl;
+	}
 };
 
 int main()
@@ -78,6 +140,57 @@ int main()
 	sol.FindParenthesis("(           )}");
 	sol.FindParenthesis("(           {}");
 	sol.FindParenthesis("([]]){{}((]]){{}((  ]]){{}((]]){{}((]]){{}((]]){{}((]]){{}(( ]]){{}((]]){{}((]]){{}((]]){{}(( }}");
+
+	{
+		vector<int> vec{ 2, 0, 3, 0, 4, 5, 0 };
+		sol.printv(vec);
+		cout << sol.move_zeroesV2(vec) << endl;
+		sol.printv(vec);
+		cout << endl;
+	}
+
+	{
+		vector<int> vec{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0 };
+		sol.printv(vec);
+		cout << sol.move_zeroesV2(vec) << endl;
+		sol.printv(vec);
+		cout << endl;
+	}
+	{
+		vector<int> vec{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0 };
+		sol.printv(vec);
+		cout << sol.move_zeroesV2(vec) << endl;
+		sol.printv(vec);
+		cout << endl;
+	}
+	{
+		vector<int> vec{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+		sol.printv(vec);
+		cout << sol.move_zeroesV1(vec) << endl;
+		sol.printv(vec);
+		cout << endl;
+	}
+	{
+		vector<int> vec{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+		sol.printv(vec);
+		cout << sol.move_zeroesV1(vec) << endl;
+		sol.printv(vec);
+		cout << endl;
+	}
+	{
+		vector<int> vec{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+		sol.printv(vec);
+		cout << sol.move_zeroesV2(vec) << endl;
+		sol.printv(vec);
+		cout << endl;
+	}
+	{
+		vector<int> vec{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+		sol.printv(vec);
+		cout << sol.move_zeroesV2(vec) << endl;
+		sol.printv(vec);
+		cout << endl;
+	}
 }
     
 
