51e0049bbbaa42acbbe7828502d7a662e2e5cdad
Jaguar Rivero <jrivero@zx1.ca>
2024-05-17 11:47:41 -0700

    Implemented Toeplitz - with vector

--- LargetrParenthesis/Practice.cpp
+++ LargetrParenthesis/Practice.cpp
@@ -60,7 +60,7 @@ struct Solution
 
 		for (size_t i = 0; i < s.size(); i++)
 		{
-			if (initialSquare == INT_MAX && s[i] == '[')
+			if (initialSquare == INT_MAX && s[i] == '	')
 			{
 				initialSquare = i;
 				continue;
@@ -1021,8 +1021,108 @@ private:
 		return 1 + max(left, right);
 	}
 };
+/*
+Given an m x n matrix, return true if the matrix is Toeplitz. Otherwise, return false.
+
+A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same elements.
+
+Example 1:
+
+1 2 3 4
+5 1 2 3
+9 5 1 2
+
+Input: matrix = [[1,2,3,4],[5,1,2,3],[9,5,1,2]]
+Output: true
+Explanation:
+In the above grid, the diagonals are:
+"[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]".
+In each diagonal all elements are the same, so the answer is True.
+
+Example 2:
+
+1 2
+2 2
+
+Input: matrix = [[1,2],[2,2]]
+Output: false
+Explanation:
+The diagonal "[1, 2]" has different elements.
 
 
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 20
+0 <= matrix[i][j] <= 99
+
+
+Follow up:
+
+What if the matrix is stored on disk, and the memory is limited such that you can only load at 
+most one row of the matrix into the memory at once?
+What if the matrix is so large that you can only load up a partial row into the memory at once?
+
+curRow = num Rows - 1 Example 3-1 = 2
+curCol = 0
+
+vector 0 = [2,0]
+vector 1 = [2,1][1,0]
+vector 2 = [2,2][1,1][0,0]
+vector 3 = [2,3][1,2][0,1]
+vector 4 = [1,3][0,2]
+vector 5 = [0,3]
+
+we need to start at the bottom left this will be our base
+go to the right, do row up, move left 
+ repeat until we reach our local top or left
+ if we reach top move right
+ if we reach left move up
+ repeat
+*/
+struct Toeplitz : public timeit
+{
+	bool isToeplitzMatrix(vector<vector<int>>& matrix) 
+	{
+		size_t numRows = matrix.size() - 1;
+		size_t numCols = matrix[0].size() - 1;
+		vector<vector<int>> diagonals;
+		vector<int> vec;
+		
+		int row = numRows;
+		int col = 0;
+
+		vec.push_back(matrix[numRows][col++]);
+		diagonals.push_back(vec);
+		printv(vec);
+		vec.clear();
+		int rowOffset{ -1 };
+		while (row > -1 && col <= numCols)
+		{
+			auto lastCol = col;
+			auto lastRow = row;
+			while (col >= 0 && row >= 0)
+			{
+				vec.push_back(matrix[row][col]);
+				row += rowOffset;
+				col--;
+			}
+			diagonals.push_back(vec);
+			printv(vec);
+			vec.clear();
+			col = ++lastCol;
+			row = lastRow;
+			if (col > numCols && row > 0)
+			{
+				col--;
+				row--;
+			}
+		}
+
+		return {};
+	}
+};
 int main()
 {
 	Solution sol;
@@ -1266,8 +1366,7 @@ int main()
 		cout << fixed << setprecision(12) <<movingAverage.next(3) << "\n"; // return 4.66667 = (1 + 10 + 3) / 3
 		cout << fixed << setprecision(12) <<movingAverage.next(5) << "\n"; // return 6.0 = (10 + 3 + 5) / 3
 	}
-#pragma endregion
-	if (true)
+	if (false)
 	{
 		if (true)
 		{
@@ -1291,6 +1390,49 @@ int main()
 			assert(val == 1);
 			cout << "result: " << val << "\n";
 
+		}
+	}
+#pragma endregion
+	if (true)
+	{
+		{
+			Toeplitz t;
+			vector<vector<int>> matrix
+			{
+				{1, 2, 3, 4},
+				{5, 1, 2, 3},
+				{9, 5, 1, 2}
+			};
+			t.isToeplitzMatrix(matrix);
+		}
+		{
+			Toeplitz t;
+			vector<vector<int>> matrix
+			{
+				{1, 2, 3},
+				{5, 1, 2},
+				{9, 5, 1}
+			};
+			t.isToeplitzMatrix(matrix);
+		}
+		{
+			Toeplitz t;
+			vector<vector<int>> matrix
+			{
+				{1, 2},
+				{5, 1},
+				{9, 5}
+			};
+			t.isToeplitzMatrix(matrix);
+		}
+		{
+			Toeplitz t;
+			vector<vector<int>> matrix
+			{
+				{1, 2},
+				{5, 1}
+			};
+			t.isToeplitzMatrix(matrix);
 		}
 	}
 }
