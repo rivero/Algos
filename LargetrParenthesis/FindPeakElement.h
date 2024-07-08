21c70e1742c94d7bd379ad593d8468dd2b8bfb91
Jaguar Rivero <658906+rivero@users.noreply.github.com>
2024-07-08 11:32:32 -0700

    Added documentation to FindPeakElement

--- LargetrParenthesis/FindPeakElement.h
+++ LargetrParenthesis/FindPeakElement.h
@@ -42,54 +42,6 @@ namespace FindPeakElement
 	Complexity of this solution: T O(log n)
 	*/
 
-
-	class SolutionNOPE 
-	{
-		// from: https://www.youtube.com/watch?v=tQpg6kcSAoY
-	public:
-		int findPeakElement(vector<int> nums) 
-		{
-			auto size = nums.size();
-			if (size == 0)
-			{
-				return {};
-			}
-			if (size == 1)
-			{
-				return 0;
-			}
-			if (size == 2)
-			{
-				return (nums[0] > nums[1]) ? 0 : 1;
-			}
-			size_t l{}, r{size -1};
-
-			while (l <= r) // less than equal because there will be a peak no worries about not finding it.
-			{
-				if (l >= size || r >= size)
-				{
-					return {};
-				}
-				auto mid = (l+r) / 2;
-				auto mid_val = nums[mid];
-
-				auto left = mid > 0 ? nums[mid - 1] : INT_MIN;
-				auto right = mid < size - 1 ? nums[mid + 1] : INT_MIN;
-
-				if ((left < mid_val) && (mid_val > right))
-				{
-					return mid;
-				}
-				else if ((left < mid_val) && (mid_val < right)) // example 1 3 5 where 3 is mid
-					l++;
-				else
-					r++;
-
-			}
-			return {};
-		}
-	};
-
 	struct Solution 
 	{
 		int findPeakElement(vector<int> nums) 
@@ -100,10 +52,13 @@ namespace FindPeakElement
 			
 			for (int i = 0; i < n; i++) 
 			{
+				// Test first element: Test if first element is greater than second element
 				if (i == 0 && nums[i] > nums[i + 1]) 
 					return i;
+				// Test last element: test of the last element is bigger than the previous
 				else if (i == n - 1 && nums[i] > nums[i - 1]) 
 					return i;
+				// Otherwise test if the current element is larger than the previous and larger than the next elements
 				else if (nums[i] > nums[i + 1] && nums[i] > nums[i - 1]) 
 					return i;
 			}
