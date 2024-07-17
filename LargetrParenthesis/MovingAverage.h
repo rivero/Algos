57a99744c2d9eeeca1853cc29d9939693890452c
Jaguar Rivero <658906+rivero@users.noreply.github.com>
2024-07-17 14:17:18 -0700

    Added docu to MovingAverage

--- LargetrParenthesis/MovingAverage.h
+++ LargetrParenthesis/MovingAverage.h
@@ -1,5 +1,9 @@
 #pragma once
 /*
+* https://leetcode.com/problems/moving-average-from-data-stream/description/
+
+346. Moving Average From Data Stream
+
 Given a stream of integers and a window size, calculate the moving average of
 all integers in the sliding window.
 
