bded20a0b14e72e8851812d1127180745c2a805b
Jaguar Rivero <658906+rivero@users.noreply.github.com>
2024-07-23 10:50:14 -0700

    Improved TopKElements

--- LargetrParenthesis/TopKElements.h
+++ LargetrParenthesis/TopKElements.h
@@ -1,66 +1,57 @@
 #pragma once
-namespace topk
-{
-	/*
-	Given a non-empty array of integers, return the k most frequent elements
+/*
+* https://leetcode.com/problems/top-k-frequent-elements/description/
 
-	Example 1:
-	k = 2
-	Input = [1,1,1,2,2,3]
-	output [1, 2]
+Given a non-empty array of integers, return the k most frequent elements
 
-	Example 2
-	k = 1
-	Input = [1]
-	output [1]
+Example 1:
+k = 2
+Input = [1,1,1,2,2,3]
+output [1, 2]
 
-	if nums = [1,1,2,2,3,3] k cannot be 2 because there are not k unique numbers
-	in this example, the only valid k is 3
+Example 2
+k = 1
+Input = [1]
+output [1]
+
+if nums = [1,1,2,2,3,3] k cannot be 2 because there are not k unique numbers
+in this example, the only valid k is 3
+
+The priority queue is a container adaptor that provides constant time lookup of the largest (by default) element, 
+at the expense of logarithmic insertion and extraction.
+
+A user-provided Compare can be supplied to change the ordering, e.g. using std::greater<T> would cause the smallest 
+element to appear as the top().
+
+
 
 	*/
-	template <typename T>
-	void TopKElements(T& c, size_t k = 2) //c = collection
+namespace TopKElements
+{
+	class Solution 
 	{
-		cout << "\n\ninput vector (k [most frequent elements]= " << k << ")" << "\n";
-		printv(c);
-
-		cout << "\nCreating map to count occurrences: <elements, occurrences>" << "\n";
-		map<int, int> counts;
-		// O(n)
-		for (auto const& elem : c)
+	public:
+		vector<int> topKFrequent(vector<int>& nums, int k)
 		{
-			counts[elem]++;
-		}
-		printm(counts);
+			map<int, int> m;
 
-		cout << "\nAdding the generated pairs to priority_queue< pair<int, int> >, "
-			"note that the pairs are sorted by the priority_queue by <first>"
-			", then <second>" << "\n";
-		priority_queue < pair<int, int> > minHeap;
-		// O(m)
-		for (auto elem : counts)
-		{
-			auto key = elem.second;
-			auto val = elem.first;
-			auto curPair = std::make_pair(key, val);
-			minHeap.push(curPair);
-		}
-
-		cout << "...priority queue looks like this: (note the sorted pairs) " << "\n";
-
-		vector<int> res;
-		for (; !minHeap.empty(); minHeap.pop())
-		{
-			auto p = minHeap.top();
-			cout << p.first << " = " << p.second << "\n";
-			if (res.size() < k)
+			for (int i = 0; i < nums.size(); i++)
 			{
-				cout << "adding to res vector: " << p.second << "\n";
-				res.push_back(p.second);
+				m[nums[i]]++;
 			}
+			priority_queue< pair <int, int> > q;
+			for (auto [elem, counter] : m)
+			{
+				q.push({ counter , elem });
+			}
+			vector<int> res;
+			while (k-- > 0)
+			{
+				auto x = q.top();
+				res.push_back(x.second); q.pop();
+			}
+			return res;
 		}
-		cout << "\nresult (Display the first k (" << k << ") elements" << "\n";
-		printv(res);
-	}
+	};
 
 }
